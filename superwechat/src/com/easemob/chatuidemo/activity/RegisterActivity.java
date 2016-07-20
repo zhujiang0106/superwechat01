631bf61e24f07f73aac5a42e118153f31eab122e
zhujiang0106 <1509574357@qq.com>
2016-07-20 11:11:26 +0800

    创建数据库users表

--- superwechat/src/com/easemob/chatuidemo/activity/RegisterActivity.java
+++ superwechat/src/com/easemob/chatuidemo/activity/RegisterActivity.java
@@ -17,7 +17,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;

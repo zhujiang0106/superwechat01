1fb02e934a19ab143350ca07082404cd35317482
zhujiang0106 <1509574357@qq.com>
2016-08-05 18:06:10 +0800

    实现登陆界面向注册界面的跳转，并且注册界面可以返回登陆界面

--- superwechat/src/com/easemob/fulicenter/activity/FuliLoginActivity.java
+++ superwechat/src/com/easemob/fulicenter/activity/FuliLoginActivity.java
@@ -3,16 +3,51 @@ package com.easemob.fulicenter.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.easemob.fulicenter.R;
+import com.easemob.fulicenter.utils.CommonUtils;
 
 public class FuliLoginActivity extends Activity {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    private String currentUsername;
+    private String currentPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuli_login);
+        usernameEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
+    }
+
+    public void onFuliLogin(View view) {
+        if (!CommonUtils.isNetWorkConnected(this)) {
+            Toast.makeText(this, R.string.network_isnot_available, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 获取当前用户输入的用户名和密码
+        currentUsername = usernameEditText.getText().toString().trim();
+        currentPassword = passwordEditText.getText().toString().trim();
+        // 如果用户名或者密码为空，则提示用户
+        if (TextUtils.isEmpty(currentUsername)) {
+            Toast.makeText(this, R.string.User_name_cannot_be_empty, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(currentPassword)) {
+            Toast.makeText(this, R.string.Password_cannot_be_empty, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, "恭喜！登陆成功!", Toast.LENGTH_SHORT).show();
+
+    }
+    public void onFuliRegister(View view) {
+        startActivityForResult(new Intent(this, FuliRegisterActivity.class), 0);
     }
 
     public void onPersonBack(View view) {

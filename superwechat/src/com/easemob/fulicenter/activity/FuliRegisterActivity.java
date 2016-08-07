01e683822060f97021413034a0eab9881715d5ad
zhujiang0106 <1509574357@qq.com>
2016-08-07 19:05:52 +0800

    实现用户注册

--- superwechat/src/com/easemob/fulicenter/activity/FuliRegisterActivity.java
+++ superwechat/src/com/easemob/fulicenter/activity/FuliRegisterActivity.java
@@ -1,17 +1,221 @@
 package com.easemob.fulicenter.activity;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.easemob.EMError;
+import com.easemob.chat.EMChatManager;
+import com.easemob.exceptions.EaseMobException;
+import com.easemob.fulicenter.FuliCenterApplication;
 import com.easemob.fulicenter.R;
+import com.easemob.fulicenter.bean.Result;
+import com.easemob.fulicenter.data.OkHttpUtils2;
+import com.easemob.fulicenter.listener.OnSetAvatarListener;
+import com.easemob.fulicenter.utils.I;
 
-public class FuliRegisterActivity extends Activity {
+import java.io.File;
 
+public class FuliRegisterActivity extends BaseActivity {
+    private EditText userNameEditText;
+    private EditText nickNameEditText;
+    private EditText passwordEditText;
+    private EditText confirmPwdEditText;
+    RelativeLayout layoutAvatar;
+    ImageView imAvatar;
+    OnSetAvatarListener mOnSetAvatarListener;
+    String avatarName;
+
+    String username;
+    String nick;
+    String pwd;
+    ProgressDialog pd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuli_register);
+        initView();
+        setListener();
+    }
+
+    private void setListener() {
+        findViewById(R.id.layout_person_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register(view);
+            }
+        });
+
+        findViewById(R.id.avatar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnSetAvatarListener = new OnSetAvatarListener(FuliRegisterActivity.this, R.id.activity_register, avatarName, I.AVATAR_TYPE_USER_PATH);
+            }
+        });
+    }
+
+    /**
+     * 注册
+     *
+     * @param view
+     */
+    private void register(View view) {
+        username = userNameEditText.getText().toString().trim();
+        nick = nickNameEditText.getText().toString().trim();
+        pwd = passwordEditText.getText().toString().trim();
+        String confirm_pwd = confirmPwdEditText.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, getResources().getString(R.string.User_name_cannot_be_empty), Toast.LENGTH_SHORT).show();
+            userNameEditText.requestFocus();
+            return;
+        } else if (!username.matches("[\\w][\\w\\d_]+")) {
+            Toast.makeText(this, getResources().getString(R.string.illegal_user_name), Toast.LENGTH_SHORT).show();
+            userNameEditText.requestFocus();
+            return;
+        } else if (TextUtils.isEmpty(nick)) {
+            Toast.makeText(this, getResources().getString(R.string.toast_nick_not_isnull), Toast.LENGTH_SHORT).show();
+            nickNameEditText.requestFocus();
+            return;
+        }else if (TextUtils.isEmpty(pwd)) {
+            Toast.makeText(this, getResources().getString(R.string.Password_cannot_be_empty), Toast.LENGTH_SHORT).show();
+            passwordEditText.requestFocus();
+            return;
+        } else if (TextUtils.isEmpty(confirm_pwd)) {
+            Toast.makeText(this, getResources().getString(R.string.Confirm_password_cannot_be_empty), Toast.LENGTH_SHORT).show();
+            confirmPwdEditText.requestFocus();
+            return;
+        } else if (!pwd.equals(confirm_pwd)) {
+            Toast.makeText(this,getResources().getString(R.string.Two_input_password),Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(pwd)) {
+            pd = new ProgressDialog(this);
+            pd.setMessage(getResources().getString(R.string.Is_the_registered));
+            pd.show();
+
+            registerAppServer();
+
+        }
+    }
+
+    /**
+     * 注册服务器账号
+     */
+    private void registerAppServer() {
+        //暂时传入后缀的jpg的图片
+        File file = new File(OnSetAvatarListener.getAvatarPath(FuliRegisterActivity.this, I.AVATAR_TYPE_USER_PATH), avatarName + I.AVATAR_SUFFIX_JPG);
+        final OkHttpUtils2<Result> utils = new OkHttpUtils2<Result>();
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,pwd)
+                .addParam(I.User.NICK,nick)
+                .targetClass(Result.class)
+                .addFile(file)
+                .execute(new OkHttpUtils2.OnCompleteListener<Result>() {
+                    @Override
+                    public void onSuccess(Result result) {
+                        Log.i("main", "result111==" + result.toString());
+                        if (!result.isRetMsg()) {
+                            registerEMServer();
+                        } else {
+                            pd.dismiss();
+                            Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        pd.dismiss();
+                    }
+                });
+    }
+
+    /**
+     * 注册环信服务器账号
+     */
+    private void registerEMServer() {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    // 调用sdk注册方法
+                    EMChatManager.getInstance().createAccountOnServer(username, pwd);
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            if (!FuliRegisterActivity.this.isFinishing())
+                                pd.dismiss();
+                            // 保存用户名
+                            FuliCenterApplication.getInstance().setUserName(username);
+                            Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registered_successfully), Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+                } catch (final EaseMobException e) {
+                    unRegisterServer();
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            if (!FuliRegisterActivity.this.isFinishing())
+                                pd.dismiss();
+                            int errorCode=e.getErrorCode();
+                            if(errorCode== EMError.NONETWORK_ERROR){
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.network_anomalies), Toast.LENGTH_SHORT).show();
+                            }else if(errorCode == EMError.USER_ALREADY_EXISTS){
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.User_already_exists), Toast.LENGTH_SHORT).show();
+                            }else if(errorCode == EMError.UNAUTHORIZED){
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.registration_failed_without_permission), Toast.LENGTH_SHORT).show();
+                            }else if(errorCode == EMError.ILLEGAL_USER_NAME){
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.illegal_user_name),Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(), getResources().getString(R.string.Registration_failed) + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void unRegisterServer() {
+        final OkHttpUtils2<Result> utils = new OkHttpUtils2<Result>();
+        utils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(Result.class)
+                .execute(new OkHttpUtils2.OnCompleteListener<Result>() {
+                    @Override
+                    public void onSuccess(Result result) {
+                        Log.e("main", result.toString());
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main", error.toString());
+                    }
+                });
+    }
+
+    private void initView() {
+        userNameEditText = (EditText) findViewById(R.id.username);
+        nickNameEditText = (EditText) findViewById(R.id.nickName);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        confirmPwdEditText = (EditText) findViewById(R.id.confirm_password);
+        layoutAvatar = (RelativeLayout) findViewById(R.id.layout_register_avatar);
+        imAvatar = (ImageView) findViewById(R.id.iv_avatar);
+        avatarName = String.valueOf(System.currentTimeMillis());
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        mOnSetAvatarListener.setAvatar(requestCode,data,imAvatar);
     }
 
     public void onPersonBack(View view) {
